c3d6f147b6ab49ae5b0aae435cce168408500b1f
SreeBadri <badri.narayanan90@gmail.com>
2018-09-27 10:52:07 -0700

    Initial commit

---
+++ EnhanceQuizStarter/EnhanceQuizStarter/QuizQuesAns.swift
@@ -0,0 +1,28 @@
+//
+//  QuizQuesAns.swift
+//  EnhanceQuizStarter
+//
+//  Created by Badri Sree on 26/09/18.
+//  Copyright © 2018 Treehouse. All rights reserved.
+//
+
+import Foundation
+import GameKit
+
+class Questions: ViewController {
+
+    let questions = ["What is 2+2?", "What is 3*6?", "What is 5+10?", "What is 5*6?", "What is 7+7?", "What is 9*2?", "What is 0+9?", "What is 7*4?", "What is 0*3?", "What is 10+12?"]
+    
+    let answers = [4,18,15,30,14,18,9,28,0,22]
+    
+
+    func randomQuestions(){
+        let  indexOfSelectedQuestion = GKRandomSource.sharedRandom().nextInt(upperBound: questions.count)
+        let theRandomQuestion = questions[indexOfSelectedQuestion]
+      questionField.text = theRandomQuestion
+        playAgainButton.isHidden = true
+        
+    }
+    
+}
+let questions = Questions()
